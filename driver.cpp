127261dfa74b3105775a224712fa168a52a49f6a
Richard Poulson <Rick.Poulson@colorado.edu>
2018-11-18 06:54:13 -0700

    Removing WebProxy Will define WebProxy from scratch using class wizard in Visual Studio

--- driver.cpp
+++ driver.cpp
@@ -5,8 +5,8 @@ int main(int argc, char **argv) {
   if (argc != 3) {
     cout << argc << endl;
     cout <<
-      "Invalid number of additional arguments, please enter one number" <<
-      "as argument." << endl;
+      "Invalid number of additional arguments, please enter two numbers (port " <<
+      "number, number threads) as argument." << endl;
       exit(EXIT_FAILURE);
   }
   WebProxy myWebProxy(atoi(argv[1]), atoi(argv[2]));
--- webproxy.cpp
+++
@@ -1,9 +0,0 @@
-#include "webproxy.h"
-
-// CONSTRUCTOR
-WebProxy::WebProxy(int port_number, int max_mumbers_threads)
-{
-}
-// DESTRUCTOR
-WebProxy::~WebProxy(){
-}
