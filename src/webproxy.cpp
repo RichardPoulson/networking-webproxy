984ef0185774e3f9cbfb98b0f6ba72e7dc98eebd
Richard Poulson <Rick.Poulson@colorado.edu>
2018-11-18 18:47:22 -0700

    Starting fresh
    Wasn't able to debug with previous setup.

---
+++ src/webproxy.cpp
@@ -0,0 +1,18 @@
+/*
+ * webproxy.cpp
+ *
+ *  Created on: Nov 18, 2018
+ *      Author: richard
+ */
+
+#include "webproxy.h"
+
+WebProxy::WebProxy() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WebProxy::~WebProxy() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/webproxy.h
@@ -0,0 +1,17 @@
+/*
+ * webproxy.h
+ *
+ *  Created on: Nov 18, 2018
+ *      Author: richard
+ */
+
+#ifndef WEBPROXY_H_
+#define WEBPROXY_H_
+
+class WebProxy {
+public:
+	WebProxy();
+	virtual ~WebProxy();
+};
+
+#endif /* WEBPROXY_H_ */
